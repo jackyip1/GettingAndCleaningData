050e7e105d560f88d7b78f0d5afc0b7d40074fe4
jackyip1 <sonyyip2001@gmail.com>
2017-03-05 17:42:56 -0500

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,71 @@
+# Set Working Directory
+
+setwd("C:/Users/Jack/Desktop/Data Scientist Specialization/UCI HAR Dataset")
+
+#1. Merge the training and the test data.
+#Read training data
+
+features        <- read.table("./features.txt",header=FALSE)
+activityLabel   <- read.table("./activity_labels.txt",header=FALSE)
+subjectTrain    <- read.table("./train/subject_train.txt", header=FALSE)
+xTrain          <- read.table("./train/X_train.txt", header=FALSE)
+yTrain          <- read.table("./train/y_train.txt", header=FALSE)
+
+#Assign column names to training data
+colnames(activityLabel)<-c("activityId","activityType")
+colnames(subjectTrain) <- "subId"
+colnames(xTrain) <- features[,2]
+colnames(yTrain) <- "activityId"
+
+#Merge training data
+trainData <- cbind(yTrain,subjectTrain,xTrain)
+
+#Read test Data
+subjectTest    <-read.table("./test/subject_test.txt", header=FALSE)
+xTest         <- read.table("./test/X_test.txt", header=FALSE)
+yTest         <- read.table("./test/y_test.txt", header=FALSE)
+
+# Assign column names to test data
+colnames(subjectTest) <- "subId"
+colnames(xTest) <- features[,2]
+colnames(yTest) <- "activityId"
+
+# Merge test data
+testData <- cbind(yTest,subjectTest,xTest)
+
+# Merge training and test data
+finalData <- rbind(trainData,testData)
+
+# 2. Extract only the measurements on the mean and standard deviation for each measurement
+colNames <- colnames(finalData);
+
+data_mean_std <-finalData[,grepl("mean|std|subId|activityId",colnames(finalData))]
+
+#3. #Uses descriptive activity names to name the activities in the data set
+library(plyr)
+
+data_mean_std <- join(data_mean_std, activityLabel, by = "activityId", match = "first")
+
+data_mean_std <-data_mean_std[,-1]
+
+#4. Appropriately labels the data set with descriptive variable names.
+#Remove parentheses
+names(data_mean_std) <- gsub("\\(|\\)", "", names(data_mean_std), perl = TRUE)
+
+#correct syntax in names
+names(data_mean_std) <- make.names(names(data_mean_std))
+
+#add descriptive names
+names(data_mean_std) <- gsub("Acc", "Acceleration", names(data_mean_std))
+names(data_mean_std) <- gsub("^t", "Time", names(data_mean_std))
+names(data_mean_std) <- gsub("^f", "Frequency", names(data_mean_std))
+names(data_mean_std) <- gsub("BodyBody", "Body", names(data_mean_std))
+names(data_mean_std) <- gsub("mean", "Mean", names(data_mean_std))
+names(data_mean_std) <- gsub("std", "Std", names(data_mean_std))
+names(data_mean_std) <- gsub("Freq", "Frequency", names(data_mean_std))
+names(data_mean_std) <- gsub("Mag", "Magnitude", names(data_mean_std))
+
+#creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+tidydata_average_sub<- ddply(data_mean_std, c("subId","activityType"), numcolwise(mean))
+
+write.table(tidydata_average_sub,file="tidydata.txt")
